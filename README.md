2c618366b14b731570c7137cb8562538dbacccfc
Paul Costanza <paulvincentcostanza@gmail.com>
2024-12-30 12:34:32 -0500

    can now grab items in form

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 A recipe book that creates recipes with the ingriedience that you have. 
 
+## Dec 30th
+- added a way to grab ingredients from button
+
 ## Dec 27th
-- created header section
-- created form section
+- created header outline
+- created form outline
 
 ## Dec 26th
-
 - Day 1!
 - Setup react project
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,35 @@
+import React from 'react'
+
 export default function Main() {
+
+    const [ingredients, setIngredient] = React.useState(["Chicken", "Oregano", "Tomatoes"])
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        setIngredient(prev => [...prev, newIngredient])
+    }
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
     return (
-        <form action="">
-            <input aria-label="Add ingredient" type="text" placeholder="e.g. chicken broth" />
-            <button>+ Add ingredient</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    aria-label="Add ingredient"
+                    type="text"
+                    placeholder="e.g. chicken broth"
+                    name="ingredient"
+                />
+                <button>+ Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </>
     )
 }
