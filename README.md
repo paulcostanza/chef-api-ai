460da6a98b22f31f5eb45c3ee5b72f3d175a949d
Paul Costanza <paulvincentcostanza@gmail.com>
2025-01-01 21:19:22 -0500

    ingredient and recipe sections have been added

--- README.md
+++ README.md
@@ -2,16 +2,26 @@
 
 A recipe book that creates recipes with the ingriedience that you have. 
 
-# Dec 31st
+#### Jan 1st, 2025
+- added ingredients list section
+- added recipe section
+
+#### Dec 31st, 2024
 - updated from React version 18.3 to 19 for some functionality in my form section
 
-## Dec 30th
+#### Dec 30th
 - added a way to grab ingredients from <code>button</code>
 
-## Dec 27th
+#### Dec 27th
 - created header outline
 - created form outline
 
-## Dec 26th
+#### Dec 26th
 - Day 1!
 - Setup react project
+
+--- 
+
+## Would like to add section
+
+- delete each individual ingredient if needed
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,9 +26,17 @@ export default function Main() {
                 />
                 <button>+ Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
         </>
     )
 }
